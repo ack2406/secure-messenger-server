4619e8e5b49546d616f18fc0eae38667cfa6351d
ack2406 <ack9222@gmail.com>
2023-05-27 23:44:57 +0200

    feat: create proper handlers

--- index.ts
+++ index.ts
@@ -10,44 +10,84 @@ const io = require("socket.io")(server, {
   },
 });
 
-interface User {
-  name: string;
-  socket: Socket;
-}
-
-let userSockets: User[] = [];
+let users: { [name: string]: Socket } = {};
 
 const onConnection = (socket: Socket) => {
-  console.log("connected");
+  console.log(`${socket.id} connected`);
 
+  // when a user logs in, we add him to the users object
   socket.on("join", (name: string) => {
-    userSockets.push({ name, socket });
-    console.log(name + " joined");
-    socket.emit("status", "connected");
+    console.log(`${socket.id} joined as ${name}`);
 
-    // send the list of users to all the users
-    const users = userSockets.map((u) => u.name);
-    io.emit("users", users);
+    users[name] = socket;
+
+    socket.emit("join", name);
   });
 
-  socket.on("message", (msg: string, user: string) => {
-    console.log(msg + " to " + user);
-    // find the user in the users array
-    const userSocket = userSockets.find((u) => u.name === user);
+  socket.on("invite", (inviter: string, invitee: string) => {
+    console.log(`"${inviter}" invited user "${invitee}"`);
 
-    if (!userSocket) {
-      console.log("user not found");
-      return;
+    // if the user is not connected, we send a message to the sender
+    if (!users[invitee]) {
+      socket.emit("unavailable");
     }
-
-    userSocket.socket.emit("message", msg, user, false);
-    socket.emit("message", msg, user, true);
+    // if the user is connected, we send a message to the sender and to the receiver
+    else {
+      users[invitee].emit("invite", inviter, invitee);
+    }
   });
 
+  socket.on("accept", (inviter: string, invitee: string) => {
+    console.log(`"${invitee}" accepted "${inviter}"`);
+
+    // we send a message to the sender and to the receiver
+    users[inviter].emit("accept", inviter, invitee);
+  });
+
+  // when invitee accepted the invitation, we send details to the inviter
+  socket.on("sendDetails", (inviter: string, invitee: string) => {
+    console.log(`"${invitee}" accepted "${inviter}"`);
+
+    // we send a message to the sender and to the receiver
+    users[invitee].emit("sendDetails", inviter);
+  });
+
+  socket.on("decline", (inviter: string, invitee: string) => {
+    console.log(`"${inviter}" declined "${invitee}"`);
+
+    // we send a message to the sender and to the receiver
+    users[inviter].emit("decline", invitee);
+  });
+
+  // when a user sends a message, we send it to the receiver and back to the sender for confirmation
+  socket.on("message", (msg: string, sender: string, receiver: string) => {
+    console.log(`"${sender}" sent "${msg}" to "${receiver}"`);
+
+    console.log(users[receiver]);
+
+    users[receiver].emit("getMessage", msg, sender);
+  });
+
+  socket.on(
+    "file",
+    (file: File, sender: string, receiver: string, fileName: string) => {
+      console.log(`"${sender}" sent file: "${file.name}" to "${receiver}"`);
+
+      users[receiver].emit("getFile", file, sender, fileName);
+    }
+  );
+
+  // when a user disconnects, we remove him from the users object
   socket.on("disconnect", () => {
-    console.log("disconnected");
-    // remove the user from the users array
-    userSockets = userSockets.filter((u) => u.socket.id !== socket.id);
+    console.log(`${socket.id} disconnected`);
+
+    // we loop through the users object to find the user that disconnected
+    for (const [name, user] of Object.entries(users)) {
+      if (user.id === socket.id) {
+        delete users[name];
+        break;
+      }
+    }
   });
 };
 
